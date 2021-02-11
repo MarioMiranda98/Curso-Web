9db325e97956712f92075616de08150b39558e03
MarioMiranda98 <farolin95@msn.com>
2021-02-11 00:27:48 -0600

    JS ok

---
+++ Javascript/js/local_storage.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    // if(typeof(Storage) !== "undefined") {
+    //     localStorage.setItem('nombre', 'Mario Miranda');
+    //     document.write('Si soporta');
+    // } else 
+    //     document.write('Tu navegador no soporta LocalStorage');
+    // document.write(localStorage.getItem('nombre'));
+    // localStorage.removeItem('nombre');
+    //------------------------- SessionStorage -------------------------//
+    // sessionStorage.setItem('nombre', 'Mario Miranda');
+    // document.write(sessionStorage.getItem('nombre'));
+});
